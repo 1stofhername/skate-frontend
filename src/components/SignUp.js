247bf187f7ff465c68e4d9eadfc5d90d3b6bc294
Alicia <arrowsrp@yahoo.com>
2022-09-01 14:31:09 -0700

    Creates handleFormChange function and adds onChange listener to all form elements

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -8,6 +8,10 @@ export default function SignUp (){
         category:null,
         });
 
+    function handleFormChange (e) {
+        console.log(`${e.target.name}: ${e.target.value}`)
+    }
+
     return (
         <div className="popover" id="sign-up">
             <h2>Sign Up</h2>
@@ -15,19 +19,19 @@ export default function SignUp (){
                 <span className="user-info">
                     <label>
                         First Name:
-                        <input type="text" name="first_name" placeholder="Chaka" />
+                        <input type="text" name="first_name" placeholder="Chaka" onChange={handleFormChange} />
                     </label>
                     <label>
                         Last Name:
-                        <input type="text" name="last_name" placeholder="Zulu" />
+                        <input type="text" name="last_name" placeholder="Zulu" onChange={handleFormChange} />
                     </label>
                     <label>
                         Email:
-                        <input type="text" name="email" placeholder="ChakaZulu@email.com" />
+                        <input type="text" name="email" placeholder="ChakaZulu@email.com" onChange={handleFormChange} />
                     </label>
                 </span>
                 <span className="category-form">
-                    {categories.map(category=>(<label key={category}><input type="radio" key={category} id={category} value={category} name="category" />{category}</label>))}
+                    {categories.map(category=>(<label key={category}><input type="radio" key={category} id={category} value={category} name="category" onChange={handleFormChange} />{category}</label>))}
                 </span>
                 <input type="submit" value="Create" />
             </form>
