75b906610a268e6d62aa2f2e44e5a9800544100d
Alicia <arrowsrp@yahoo.com>
2022-09-01 11:28:44 -0700

    Deletes default App.js content, calls ProfileBar in App.js

---
+++ src/components/ProfileBar.js
@@ -0,0 +1,11 @@
+
+export default function ProfileBar () {
+    return (
+        <div className="bar" id="profile-bar">
+            <img src={null} />
+            <p>Hello, first_name!</p>
+            <p>Skate Park Name</p>
+            <p>Rollerskates</p>
+        </div>
+    )
+}
