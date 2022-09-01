a4e05e8c139f3b0fc59a268f4c07f6613f1ec5c6
Alicia <arrowsrp@yahoo.com>
2022-09-01 12:00:08 -0700

    Adds loginstatus variable and logic

--- src/App.js
+++ src/App.js
@@ -1,22 +1,27 @@
 import { useState, useEffect } from 'react';
 import './css/App.css';
 import ProfileBar from './components/ProfileBar';
+import Login from './components/Login';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(
-    localStorage.getItem('isLoggedIn') === 'true'
-  );
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const loginStatus = (localStorage.getItem('isLoggedIn'));
 
   useEffect(() =>{
     localStorage.setItem('isLoggedIn', isLoggedIn);
   },[isLoggedIn]); 
 
+  const handleLogIn = () => {
+    setIsLoggedIn(true);
+  }
+
   return (
+    loginStatus === "true"?
     <div className="App">
       <header className="App-header">
        <ProfileBar />
       </header>
-    </div>
+    </div>:<Login handleLogIn={handleLogIn} />
   );
 }
 
