e68d933a1c03753a6f11544d8893f5e3031376dc
Alicia <arrowsrp@yahoo.com>
2022-09-01 11:45:17 -0700

    Initializes isLoggedIn state and saves to local storage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './css/App.css';
 import ProfileBar from './components/ProfileBar';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(
+    localStorage.getItem('isLoggedIn') === 'true'
+  );
+
+  useEffect(() =>{
+    localStorage.setItem('isLoggedIn', isLoggedIn);
+  },[isLoggedIn]); 
+
   return (
     <div className="App">
       <header className="App-header">
