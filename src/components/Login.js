c8d4d457b99cf19925a0dfe3e6369832721b7e43
Alicia <arrowsrp@yahoo.com>
2023-05-21 18:31:50 -0700

    Moves form toggle buttons and header outside of login form and adds to 'form parent' div.

--- src/components/Login.js
+++ src/components/Login.js
@@ -25,22 +25,29 @@ export default function Login ({ handleLogin, onSignUpClick, error, isLoggingIn 
                 <div className='image-container'>
                 </div>
                 <div className='form-container'>
-                <form className="form" id="sign-up-form" onSubmit={handleSubmit}>
-                    <legend>Skate.</legend>
-                    <div className='toggle-buttons-container'>
-                        <button className='form-button' id={isLoggingIn?"selected":null}>Login</button>
-                        <button className='form-button' onClick={onSignUpClick}>Sign Up</button>
+                    <div className='form header'>
+                        <h2>Skate.</h2>
+                        <div className='form-toggle-button-container'>
+                            <button className='form-button' id={isLoggingIn?"selected":null}>Login</button>
+                            <button className='form-button' onClick={onSignUpClick}>Sign Up</button>
+                        </div>
                     </div>
-                    <div className='form-fields-container'>
-                        <input type="text" name="email" placeholder="email" onChange={handleEmailChange}/>
-                    
-                        <input type="password" name="password" placeholder="password" onChange={handlePasswordChange} />
-                    </div>
-                    <div>
-                        <input className='form-button' type="submit" value="submit" />
-                    </div>
-                    {error? <p className='error-message'>{error}</p>:null}
-                </form>
+                    <form className="form" id="sign-up-form" onSubmit={handleSubmit}>
+                        {/* <legend>Skate.</legend>
+                        <div className='toggle-buttons-container'>
+                            <button className='form-button' id={isLoggingIn?"selected":null}>Login</button>
+                            <button className='form-button' onClick={onSignUpClick}>Sign Up</button>
+                        </div> */}
+                            <div className='form-fields-container'>
+                            <input type="text" name="email" placeholder="email" onChange={handleEmailChange}/>
+                        
+                            <input type="password" name="password" placeholder="password" onChange={handlePasswordChange} />
+                        </div>
+                        <div>
+                            <input className='form-button' type="submit" value="submit" />
+                        </div>
+                        {error? <p className='error-message'>{error}</p>:null}
+                    </form>
                 <span>
                 {/* <button onClick={onSignUpClick}>Sign Up.</button> */}
                 </span>
